66cb694b1c2e867a12a5862a324ed3e388aa9d24
PradeepJaiswar <mado023@gmail.com>
2017-02-13 21:49:55 +0530

    added api based connection

--- demo/app.js
+++ demo/app.js
@@ -1,82 +1,99 @@
+var http = require('http');
 var express = require('express');
 var app = express();
 var expressWs = require('express-ws')(app);
-var os = require('os');
 var pty = require('node-pty');
 var cors = require('cors');
 app.use(cors());
 app.options('*', cors());
+var terminals = ''; //global terminals
 
-var terminals = '',
-    logs = '';
+/*
+##########################################################
+###################FOR TERMINAL DEMO ONLY ################
+##########################################################
+*/
+  app.use('/build', express.static(__dirname + '/../build'));
 
-app.use('/build', express.static(__dirname + '/../build'));
-
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
-
-app.get('/style.css', function(req, res){
-  res.sendFile(__dirname + '/style.css');
-});
-
-app.get('/main.js', function(req, res){
-  res.sendFile(__dirname + '/main.js');
-});
-
-app.ws('/notify', function(ws, req) {
-  ws.on('message', function(msg) {
-    console.log(msg);
-    ws.send(msg);
+  app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
   });
-});
+
+  app.get('/style.css', function(req, res){
+    res.sendFile(__dirname + '/style.css');
+  });
+
+  app.get('/main.js', function(req, res){
+    res.sendFile(__dirname + '/main.js');
+  });
+/*
+##########################################################
+###################FOR TERMINAL DEMO ONLY ################
+##########################################################
+*/
+
+/*
+##########get user info and .pem file path###############
+*/
+function getTerm(token) {
+    return new Promise((resolve, reject) => {
+      return http.get({
+          host: '35.154.96.42',
+          port : 7000,
+          path: '/api/v1/users/me?token=' + token
+      }, function(response) {
+          // Continuously update stream with data
+          var body = '';
+          response.on('data', function(d) {
+              body += d;
+          });
+          response.on('end', function() {
+              return resolve(JSON.parse(body));
+          });
+      });
+    })
+}
+
+/*
+##########xterm over WebSocket###############
+##########do not edit code below if you are not sure & know what you are doing ###############
+*/
 
 app.ws('/terminals/:pid', function (ws, req) {
-  if(terminals[req.params.pid]){
-    var term = terminals[req.params.pid];
-  }else {
-    var term = pty.spawn('ssh', ["-i", "/home/ec2-user/xterm.js/demo/githubdemo.pem", "ec2-user@35.154.96.42"], {
+  getTerm(req.params.pid)
+    .then(user_info => {
+      if(terminals[req.params.pid]){
+        var term = terminals[req.params.pid];
+      }else {
+        var term = pty.spawn('ssh', ["-i", "/Users/pradeepjaiswar/workspace/greyatom/server/xterm-server/demo/githubdemo.pem", "ec2-user@35.154.96.42"], {
           name: 'xterm-color',
           cwd: process.env.PWD,
           env: process.env
         });
+        terminals[req.params.pid] = term;
+      }
 
-    // FOR LOCAL enable this and commment above 5 line
-    // var term = pty.spawn(process.platform === 'win32' ? 'cmd.exe' : 'bash', [], {
-    //       name: 'xterm-color',
-    //       cwd: process.env.PWD,
-    //       env: process.env
-    //     });
+      term.on('data', function(data) {
+        ws.send(data);
+      });
 
-    terminals[req.params.pid] = term;
-    logs[req.params.pid] = '';
+      ws.on('message', function(msg) {
+        term.write(msg);
+      });
 
-    term.on('data', function(data) {
-      logs[req.params.pid] += data;
-    });
-  }
-
-  ws.send(logs[req.params.pid]);
-
-  term.on('data', function(data) {
-    try {
-      ws.send(data);
-    } catch (ex) {
-      // The WebSocket is not open, ignore
-    }
-  });
-
-  ws.on('message', function(msg) {
-    term.write(msg);
-  });
-
-  ws.on('close', function () {
-    process.kill(term.pid);
-    delete terminals[req.params.pid];
-    delete logs[req.params.pid];
-  });
-
+      ws.on('close', function () {
+        // process.kill(term.pid);
+        // delete terminals[req.params.pid];
+        // delete logs[req.params.pid];
+      });
+    })
+    .catch(err => {
+      // console.log(err);
+    })
 });
 
+/*
+##########listen server at port 30000################
+*/
 console.log('App listening to *:3000');
 app.listen(3000);
