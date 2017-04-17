a6bf7b1e9852db156412ddf760d8ed9b2adbb626
PradeepJaiswar <mado023@gmail.com>
2017-04-17 12:43:57 +0530

    add try catch block around api and websocket

--- demo/app.js
+++ demo/app.js
@@ -37,20 +37,26 @@ var terminals = ''; //global terminals
 */
 function getTerm(token) {
     return new Promise((resolve, reject) => {
-      return http.get({
-          host: 'api.greyatom.com',
-          path: '/v1/users/' + token,
-          headers: {'access-token': token}
-      }, function(response) {
-          // Continuously update stream with data
-          var body = '';
-          response.on('data', function(d) {
-              body += d;
-          });
-          response.on('end', function() {
-              return resolve(JSON.parse(body));
-          });
-      });
+      try {
+        return http.get({
+            host: 'api.greyatom.com',
+            path: '/v1/users/' + token,
+            headers: {'access-token': token}
+        }, function(response) {
+            // Continuously update stream with data
+            var body = '';
+            response.on('data', function(d) {
+                body += d;
+            });
+            response.on('end', function() {
+                return resolve(JSON.parse(body));
+            });
+        });
+      } catch (err) {
+        console.log('Api failed');
+        console.log(err);
+        reject;
+      }
     })
 }
 
@@ -59,38 +65,43 @@ function getTerm(token) {
 ##########Do not edit code below if you are not sure & know what you are doing ###############
 */
 app.ws('/terminals/:pid', function (ws, req) {
-  getTerm(req.params.pid)
-    .then(user_info => {
-      console.log(user_info);
-      if(terminals[req.params.pid]){
-        var term = terminals[req.params.pid];
-      }else {
-        // var term = pty.spawn('ssh', ["-i", user_info.pem_file, user_info.user_host], {
-        var term = pty.spawn('sudo', ['su','-',user_info.data.username], {
-          name: 'xterm-color',
-          cwd: process.env.PWD,
-          env: process.env
+  try {
+    getTerm(req.params.pid)
+      .then(user_info => {
+        console.log(user_info);
+        if(terminals[req.params.pid]){
+          var term = terminals[req.params.pid];
+        }else {
+          // var term = pty.spawn('ssh', ["-i", user_info.pem_file, user_info.user_host], {
+          var term = pty.spawn('sudo', ['su','-',user_info.data.username], {
+            name: 'xterm-color',
+            cwd: process.env.PWD,
+            env: process.env
+          });
+          terminals[req.params.pid] = term;
+        }
+
+        term.on('data', function(data) {
+          ws.send(data);
         });
-        terminals[req.params.pid] = term;
-      }
 
-      term.on('data', function(data) {
-        ws.send(data);
-      });
+        ws.on('message', function(msg) {
+          term.write(msg);
+        });
 
-      ws.on('message', function(msg) {
-        term.write(msg);
-      });
-
-      ws.on('close', function () {
-        // process.kill(term.pid);
-        // delete terminals[req.params.pid];
-        // delete logs[req.params.pid];
-      });
-    })
-    .catch(err => {
-      // console.log(err);
-    })
+        ws.on('close', function () {
+          // process.kill(term.pid);
+          // delete terminals[req.params.pid];
+          // delete logs[req.params.pid];
+        });
+      })
+      .catch(err => {
+        // console.log(err);
+      })
+  } catch (err) {
+      console.log('Terminal webSocket failed');
+      console.log(err);
+  }
 });
 
 /*
