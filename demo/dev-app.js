495d5576ae3f35668c5810cc52118139f7110b2a
PradeepJaiswar <mado023@gmail.com>
2017-09-28 14:53:46 +0530

    Update dev-app.js

--- demo/dev-app.js
+++ demo/dev-app.js
@@ -64,7 +64,7 @@ function getTerm(user_id,user_token) {
 */
 app.ws('/terminals/:user_id/:user_token', function (ws, req) {
   try {
-    getTerm(req.params.user_id, req.params.token)
+    getTerm(req.params.user_id, req.params.user_token)
       .then(user_info => {
         var term = pty.spawn('sudo', ['su','-',user_info.data.userName], {
           name: 'xterm-color',
